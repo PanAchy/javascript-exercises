d70a1d03064d9733f5d56074cf0ff560da2a2499
Pan Odin S <19510452+PanAchy@users.noreply.github.com>
2024-02-26 16:10:28 -0600

    Beginning of assignments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let reverseInput = "";
+  for (let step = input.length - 1; step >= 0; step--) {
+    reverseInput = reverseInput.concat("", input[step]);
+  }
+  return reverseInput;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, value) {
+  var emptyArray = [];
+  var valueArray = emptyArray.push(value);
+  console.log(valueArray);
+  console.log(valueArray.length);
+  for (let x = 0; x < valueArray.length; x++) {
+    array.splice(array.indexOf(valueArray[x], 1));
+    // console.log(array);
+    // console.log("doqhdqowdjqwdq");
+  }
+  //   array.splice(array.indexOf(value), 1);
+  //   console.log(array);
+  return array;
 };
 
 // Do not edit below this line
